8bde3fc7652508656b07860488c823b88f55daf8
Kevin Sandermann <kevin.sandermann@partner.bmwgroup.com>
2018-05-03 15:38:34 +0200

    hotfix: add role to user before checking helm version

--- install_tiller_on_openshift.sh
+++ install_tiller_on_openshift.sh
@@ -9,11 +9,11 @@ oc project $TILLER_NAMESPACE
 
 oc process -f https://github.com/openshift/origin/raw/master/examples/helm/tiller-template.yaml -p TILLER_NAMESPACE="${TILLER_NAMESPACE}" | oc create -f -
 
+oc policy add-role-to-user edit "system:serviceaccount:${TILLER_NAMESPACE}:tiller"
+
 oc rollout status deployment tiller
 
 helm version
-
-oc policy add-role-to-user edit "system:serviceaccount:${TILLER_NAMESPACE}:tiller"
 
 helm install https://github.com/jim-minter/nodejs-ex/raw/helm/helm/nodejs-0.1.tgz -n nodejs-ex
 
